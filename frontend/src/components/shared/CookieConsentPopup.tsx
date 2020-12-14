f7f6e13fde92f99dc0a15d2f0cfc21a89ded9d18
ggbill <bill@goinggone.io>
2020-12-14 11:53:32 +0000

    remove get me out of here on the cookie consent

--- frontend/src/components/shared/CookieConsentPopup.tsx
+++ frontend/src/components/shared/CookieConsentPopup.tsx
@@ -7,28 +7,19 @@ import CookieGif from '../../images/cookie.gif'
 const CookieConsentPopup = () => {
     return (
         <CookieConsent
-            enableDeclineButton
             style={{
                 background: "white",
                 color: "#222",
                 boxShadow: "0px -3px 10px rgba(50, 50, 50, 0.26)",
             }}
             location="bottom"
-            buttonText="I'm OK with that"
-            declineButtonText="No thanks, get me out of here"
-            cookieName="myAwesomeCookieName2"
+            buttonText="OK"
+            cookieName="tmbCookieConsent"
             buttonClasses="accept-button"
-            declineButtonClasses="decline-button"
             containerClasses="cookie-consent"
             contentClasses="cookie-content"
             buttonWrapperClasses="button-wrapper"
-            expires={1}
-            // onAccept={() => {
-            //     alert("Accept was triggered by clicking the Accept button");
-            // }}
-            onDecline={() => {
-                window.location.href = "https://www.youtube.com/watch?v=UQU4sIn96M4&ab_channel=MyMotownTunes0815007";
-            }}
+            expires={7}
         >
             <div className="cookie-content-wrapper">
                 <img src={CookieGif} alt="cookie-gif" />
