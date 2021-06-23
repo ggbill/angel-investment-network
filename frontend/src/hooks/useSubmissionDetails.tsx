3971042f08496059035fcc0cb8f1f7c490e6f278
ggbill <bill@goinggone.io>
2021-06-23 10:35:11 +0100

    write to AIN DB

--- frontend/src/hooks/useSubmissionDetails.tsx
+++ frontend/src/hooks/useSubmissionDetails.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import useFetch from './useFetch'
 
-
 const useSubmissionDetails = () => {
 
     const [submissionDetails, setSubmissionDetails] = useState<App.SubmissionDetails>({
